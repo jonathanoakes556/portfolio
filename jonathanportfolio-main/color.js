8eed25370189f28b9579406af9f38230c2147da3
jonathanoakes556 <119904567+jonathanoakes556@users.noreply.github.com>
2023-11-30 15:14:59 -0500

    Add files via upload

---
+++ jonathanportfolio-main/color.js
@@ -0,0 +1,8 @@
+let colorpicker = document.getElementById('colorpicker');
+
+
+
+  setInterval(()=>{
+      let color = colorpicker.value;
+      document.body.style.backgroundColor = color;
+  }, 200);
